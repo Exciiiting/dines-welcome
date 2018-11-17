10fe49ada4894684f9268455aa18a178af494d57
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-17 08:22:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
  
 client.on('guildMemberAdd',async member => {
   if(member.guild.id !== '499633648866164737') return;
